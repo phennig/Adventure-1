43fe352a3ea0ed3eff862b362097f6a33700d09e
tmroz <tom.mroz@d214.org>
2014-05-13 16:02:02 -0500

    Newest edition 2

---
+++ Adventure/ClearUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClearUpViewController.h
+//  Adventure
+//
+//  Created by Thomas M. Mroz on 5/13/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClearUpViewController : UIViewController
+
+@end
---
+++ Adventure/EatViewController.h
@@ -0,0 +1,13 @@
+//
+//  EatViewController.h
+//  Adventure
+//
+//  Created by Thomas M. Mroz on 5/13/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EatViewController : UIViewController
+
+@end
